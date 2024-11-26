0b9370506130759dc2c581fa29d99c1acbbc626d
DNSO0 <toxicthrashd@gmail.com>
2024-11-26 08:49:13 -0500

    Se añade el header como componente con estilos basicos

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './componentes/Header/Header.js';
 
 function App() {
+  
   return (
-    <div className="App">
+    <div className="App">      
+      <Header />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
