dab7e54413713b5750c6e349f8f84892746460b6
DNSO0 <toxicthrashd@gmail.com>
2024-11-28 09:37:56 -0500

    Se añade el componente Campo texto al formulario y se añade estilos

--- src/componentes/formulario/Formulario.js
+++ src/componentes/formulario/Formulario.js
@@ -1,9 +1,14 @@
 import "./Formulario.css"
+import CampoTexto from "../CampoTexto/CampoTexto"
 
 const Formulario = () => {
     return <section className="formulario">
                 <from className="form">
                     <h2>Rellena el formulario para crear el colaborador.</h2>
+                    <CampoTexto />
+                    <CampoTexto />
+                    <CampoTexto />
+                    
                 </from>
            </section>
 } 
