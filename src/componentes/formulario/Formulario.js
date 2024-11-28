b42b54aac295e2d76baaa356cdc4778c4b11c4f2
DNSO0 <toxicthrashd@gmail.com>
2024-11-28 08:21:43 -0500

    Se inicia la seccion Formulario

---
+++ src/componentes/formulario/Formulario.js
@@ -0,0 +1,11 @@
+import "./Formulario.css"
+
+const Formulario = () => {
+    return <section className="formulario">
+                <from className="form">
+                    <h2>Rellena el formulario para crear el colaborador.</h2>
+                </from>
+           </section>
+} 
+
+export default Formulario
