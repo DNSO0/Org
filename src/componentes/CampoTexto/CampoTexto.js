1f83021acd8f7ef45058f09e077806d61b27a940
DNSO0 <toxicthrashd@gmail.com>
2024-11-28 17:58:58 -0500

    Implementación de props en componentes de React y modificación de estilos en el formulario

--- src/componentes/CampoTexto/CampoTexto.js
+++ src/componentes/CampoTexto/CampoTexto.js
@@ -1,9 +1,10 @@
 import "./CampoTexto.css"
 
-const CampoTexto = ()=> {
+const CampoTexto = (props)=> {
+    const placeholderModificado = `${props.placeholder}...`
     return <div className="campo-texto">
-        <label>Nombre:</label>
-        <input type="text" name="nombre" placeholder="Ingresar nombre"/>
+        <label>{props.titulo}:</label>
+        <input placeholder={placeholderModificado}/>
     </div>
 }
 
