41937335d66386167543806429f9368ad057eac8
DNSO0 <toxicthrashd@gmail.com>
2024-11-29 08:00:37 -0500

    Implementar manejo de eventos en formularios con onSubmit

--- src/componentes/formulario/Formulario.js
+++ src/componentes/formulario/Formulario.js
@@ -4,8 +4,13 @@ import ListaOpciones from "../ListaOpciones"
 import Boton from "../Boton/index"
 
 const Formulario = () => {
+
+    const manejarEnvio = (event) => {
+        event.preventDefault()
+    }
+
     return <section className="formulario">
-                <from className="form">
+                <from onSubmit={manejarEnvio} className="form">
                     <h2>Rellena el formulario para crear el colaborador.</h2>
                     <CampoTexto titulo="Nombre" placeholder="Ingresar nombre"/>
                     <CampoTexto titulo="Puesto" placeholder="Ingresar puesto"/>
