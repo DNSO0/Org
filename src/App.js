f396d3ad11190cfb936c33efb1d95e20e1fee964
DNSO0 <toxicthrashd@gmail.com>
2024-11-30 08:00:45 -0500

    Implementar el estado para mostrar/ocultar el formulario en el componente App

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
+import { useState } from 'react';
 import './App.css';
 import Header from './componentes/Header/Header.js';
 import Formulario from './componentes/formulario/Formulario.js';
 import MiOrg from './componentes/MiOrg/index.js';
 
 function App() {
+  const [mostrarFormulario, actualizarMostrar] = useState(true)
+  const cambiarMostrar =()=> {
+    actualizarMostrar(!mostrarFormulario)
+  }
   
   return (
     <div >      
       <Header />
-      <Formulario />
-      <MiOrg />
+      {/* {mostrarFormulario ? <Formulario /> : <></>} */}
+      {mostrarFormulario && <Formulario />}      
+      <MiOrg cambiarMostrar={cambiarMostrar} />
     </div>
   );
 }
--- src/componentes/MiOrg/index.js
+++ src/componentes/MiOrg/index.js
@@ -1,9 +1,19 @@
+import { useState } from "react"
 import "./MiOrg.css"
 
-const MiOrg = ()=> {
+const MiOrg = (props)=> {
+    //Estado -hooks
+    //useState - useState()
+    //const [nombreVariable, funcionActualiza] = useState(valor inicial)
+
+    // const [mostrar, actualizarMostrar] = useState(true)
+    // const manejarClick = () =>{
+    //     actualizarMostrar(!mostrar)
+    // }
+
     return <section className="orgSection">
         <h3 className="title">Mi organización</h3>
-        <img src="/img/add.png" alt="add" />
+        <img src="/img/add.png" alt="add" onClick={props.cambiarMostrar} />
     </section>
 }
 
