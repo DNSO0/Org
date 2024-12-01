28117b3c81901b11b4f9767d1db13118cb378c50
DNSO0 <toxicthrashd@gmail.com>
2024-12-01 09:01:32 -0500

    Implementar control de formularios utilizando useState y componentes controlados

--- src/componentes/formulario/Formulario.js
+++ src/componentes/formulario/Formulario.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./Formulario.css"
 import CampoTexto from "../CampoTexto/CampoTexto"
 import ListaOpciones from "../ListaOpciones"
@@ -5,16 +6,26 @@ import Boton from "../Boton/index"
 
 const Formulario = () => {
 
+    const [nombre, actualizarNombre] = useState("")
+    const [puesto, actualizarPuesto] = useState("")
+    const [foto, actualizarFoto] = useState("")
+
     const manejarEnvio = (event) => {
         event.preventDefault()
+        let datosAEnviar = {
+            nombre,
+            puesto,
+            foto
+        }
+        console.log(datosAEnviar)
     }
 
     return <section className="formulario">
                 <from onSubmit={manejarEnvio} className="form">
                     <h2>Rellena el formulario para crear el colaborador.</h2>
-                    <CampoTexto titulo="Nombre" placeholder="Ingresar nombre" required={true} />
-                    <CampoTexto titulo="Puesto" placeholder="Ingresar puesto" required />
-                    <CampoTexto titulo="Foto" placeholder="Ingresar enlace de foto" required />
+                    <CampoTexto titulo="Nombre" placeholder="Ingresar nombre" required={true} valor={nombre} actualizarValor={actualizarNombre}/>
+                    <CampoTexto titulo="Puesto" placeholder="Ingresar puesto" required valor={puesto} actualizarValor={actualizarPuesto} />
+                    <CampoTexto titulo="Foto" placeholder="Ingresar enlace de foto" required valor={foto} actualizarValor={actualizarFoto} />
                     <ListaOpciones />
                     <Boton>
                         Crear
