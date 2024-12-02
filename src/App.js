81b6ce437c8a98f4562cdc8837c1c4ef5af92984
DNSO0 <toxicthrashd@gmail.com>
2024-12-02 18:56:44 -0500

    Crear componente de equipos con propiedades dinámicas, aplicar estilos en línea y utilizar destructuración

--- src/App.js
+++ src/App.js
@@ -11,20 +11,60 @@ function App() {
     actualizarMostrar(!mostrarFormulario)
   }
   
+//Lista de equipos
+const equipos = [
+
+  {
+    titulo: "Programación",
+    colorPrimario: "#57C278",
+    colorSecundario: "#D9F7E9"
+  },
+  {
+    titulo: "Front-end",
+    colorPrimario: "#82CFFA",
+    colorSecundario: "#E8F8FF"
+  },
+  {
+    titulo: "Data Science",
+    colorPrimario: "#A6D157",
+    colorSecundario: "#F0F8E2"
+  },
+  {
+    titulo:  "Devops",
+    colorPrimario: "#E06B69",
+    colorSecundario: "#FDE7E8"
+  },
+  {
+    titulo: "UX y Diseño",
+    colorPrimario: "#DB6EBF",
+    colorSecundario: "#FAE9F5"
+  },
+  {
+    titulo: "Movil",
+    colorPrimario: "#FFBA05",
+    colorSecundario: "#FFF5D9"
+  },
+  {
+    titulo: "Innovación y  Gestión",
+    colorPrimario: "#FF8A29",
+    colorSecundario: "#FFEEDF"
+  }
+  
+]
+
   return (
     <div >      
       <Header />
       {/* {mostrarFormulario ? <Formulario /> : <></>} */}
       {mostrarFormulario && <Formulario />}      
       <MiOrg cambiarMostrar={cambiarMostrar} />
-      <Equipo equipo="Programación" />
-      <Equipo equipo="(Front End)" />
-      <Equipo equipo="(Data Science)" />
-      <Equipo equipo="(Devops)" />
-      <Equipo equipo="(UX y Diseño)" />
-      <Equipo equipo="(Móvil)" />
-      <Equipo equipo="Programación" />
-      <Equipo equipo="(Innovación y  Gestión)" />
+    
+      {
+        equipos.map ( (equipo)=>{
+          return <Equipo datos={equipo} key={equipo.titulo} />
+        } )
+      }    
+
     </div>
   );
 }
