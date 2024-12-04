a6e550039cd7c509cb2fccdad26f934dcdfbe1d4
DNSO0 <toxicthrashd@gmail.com>
2024-12-04 08:45:35 -0500

    Edición de iconos en el Footer

--- src/componentes/Footer/index.jsx
+++ src/componentes/Footer/index.jsx
@@ -1,17 +1,23 @@
 import "./Footer.css"
+import { FaXTwitter } from "react-icons/fa6";
+import { FaGithub } from "react-icons/fa";
+
 
 const Footer = () => {
     return <footer className="footer" style={{backgroundImage:"url(/img/Footer.png)"}}>
     <div className="redes">
-    <a href='https://www.aluracursos.com/'> 
-        <img src="/img/facebook.png" alt="Facebook"/>
-    </a>
-    <a href='https://www.aluracursos.com/'> 
-        <img src="/img/twitter.png" alt="twitter" />
-    </a>
-    <a href='https://www.aluracursos.com/'> 
-        <img src="/img/instagram.png" alt="instagram" />
-    </a>
+        <a href='https://www.aluracursos.com/'> 
+            <img src="/img/facebook.png" alt="Facebook"/>
+        </a>
+        <a href='https://www.aluracursos.com/'>
+            <img src="/img/instagram.png" alt="Instagram"/>
+        </a>
+        <a href="https://www.aluracursos.com/">
+        <FaXTwitter className="icon" /> 
+        </a>     
+        <a href="https://github.com/DNSO0">
+            <FaGithub className="icon" />
+        </a>         
     </div>
     <img src='/img/Logo.png' alt='org' /> 
     <strong>Desarrollado por Santiago Forero</strong>
