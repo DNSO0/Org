fb4e4f3a5f132cf23ae082937f65605e765b1011
DNSO0 <toxicthrashd@gmail.com>
2024-12-09 07:27:36 -0500

    Se añade la funcionalidad de eliminar colaborador

--- src/componentes/Equipo/index.js
+++ src/componentes/Equipo/index.js
@@ -28,7 +28,7 @@ const Equipo = (props) => {
                 <h3 style={estiloTitulo}>{titulo}</h3>
                 <div className="colaboradores">
                     {
-                        colaboradores.map ( (colaborador, index)=> <Colaborador datos={colaborador} key={index} colorPrimario={colorPrimario} /> )
+                        colaboradores.map ( (colaborador, index)=> <Colaborador datos={colaborador} key={index} colorPrimario={colorPrimario} eliminarColaborador={eliminarColaborador} /> )
                     }            
                 </div>
             </section>
